8b0e3d356909e483b34fc2ec6bf05d9115975022
idlviv <idlviv@rambler.ru>
2017-03-06 13:04:38 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  p: any;
 
   constructor() {
-    this.name = 'Angular2'
+    this.name = 'Angular2';
+    // this.p = document.querrySelector('#bs-example-navbar-collapse-1');
   }
+//   $('.navbar-collapse').on('click', function () {
+//   $('.navbar-collapse').collapse('hide');
+//   $a = $($(this).attr('href'));
+//   $('html,body').animate({ scrollTop: $a.offset().top - 50}, 500);
+//   return false;
+// });
+  
+//   this.p.addEventListener('click', function() {
+//     this.p.classlist.remove('in');
+// })
+  // $('#navbar-collapse').removeClass('in');
 }
 
