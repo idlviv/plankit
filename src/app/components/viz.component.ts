e737dac537f0926c2ac2050ead4f8682447929b5
idlviv <idlviv@rambler.ru>
2017-03-06 20:04:37 +0200

    with pics

--- src/app/components/viz.component.ts
+++ src/app/components/viz.component.ts
@@ -24,37 +24,43 @@ export class VizComponent  {
         title: 'tab0',
         text: 'Bla bla bla',
         show: true,
-        active: true
+        active: true,
+        src: './assets/greyjoy.jpg',
       },
       {
         title: 'tab1',
         text: 'Bla bla bla',
         show: false,
         active: true,
+        src: './assets/stark.jpg',
       },
       {
         title: 'tab2',
         text: 'Bla bla bla',
         show: false,
         active: true,
+        src: './assets/lannister.jpg',
       },
       {
         title: 'tab3',
         text: 'Bla bla bla',
         show: false,
-        active: true
+        active: true,
+        src: './assets/baratheon.jpg',
       },
       {
         title: 'tab4',
         text: 'Bla bla bla',
         show: false,
         active: true,
+        src: './assets/reed.jpg',
       },
       {
         title: 'tab5',
         text: 'Bla bla bla',
         show: false,
         active: true,
+        src: './assets/selmy.jpg',
       },
     ]
   }
